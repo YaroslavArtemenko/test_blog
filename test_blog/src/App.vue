0efb5f9a2f12069dfc077c66cd808a582853906f
Yaros <Yaroslavhik@yandex.ua>
2021-10-20 17:29:07 +0200

    added articles and search

--- test_blog/src/App.vue
+++ test_blog/src/App.vue
@@ -2,18 +2,25 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <Search></Search>
+
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import Search from "./components/Search";
+
+
+
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    Search
   },
 
   data: () => ({
