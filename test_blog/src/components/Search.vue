4c7143c835dbe8652a412a44d094b11b639a2d9d
Yaros <Yaroslavhik@yandex.ua>
2021-10-21 11:34:16 +0200

    added test data for all templates

--- test_blog/src/components/Search.vue
+++ test_blog/src/components/Search.vue
@@ -201,13 +201,14 @@ export default {
         {
           category: 'Meal',
           name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: '8%',
-          iron: '1%',
+          previewText: '          calories: 237,\n' +
+              '          fat: 9.0,\n' +
+              '          carbs: 37,\n' +
+              '          protein: 4.3,\n' +
+              '          sodium: 129',
+          mainText: 'Ice cream sandwiches are as much a part of summer as rain is to spring and bicycles to childhood.\n' +
+              '\n' +
+              'These classic hot-weather treats remind me of drinking from the hose, piles of bikes discarded in front yards, sitting on the front porch, and licking the sticky, sweet chocolate cake from my fingertips, while creamy vanilla ice cream melts down my wrist.'
         },
         {
           category: 'Sport',
@@ -220,24 +221,23 @@ export default {
         {
           category: 'Meal',
           name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: '3%',
-          iron: '8%',
+          previewText: ' Calories: 305,\n' +
+              '          Fat: 3.7,\n' +
+              '          Carbs: 67,\n' +
+              '          Protein: 4.3,\n' +
+              '          Sodium: 413',
+          mainText: 'Bake these easy vanilla cupcakes in just 35 minutes. Perfect for birthdays, picnics or whenever you fancy a sweet treat, they\'re sure to be a crowd-pleaser'
+
         },
         {
           category: 'Meal',
           name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: '7%',
-          iron: '16%',
+          previewText: ' calories: 356,\n' +
+              '          fat: 16.0,\n' +
+              '          carbs: 49,\n' +
+              '          protein: 3.9,\n' +
+              '          sodium: 327',
+          mainText: 'Here is my favorite gingerbread cookies recipe and one of the most popular Christmas cookie recipes on this website. Soft in the centers, crisp on the edges, perfectly spiced, molasses and brown sugar-sweetened holiday goodness. '
         },
       ],
     }
