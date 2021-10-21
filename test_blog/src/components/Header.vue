fadf26717d96baca352391b69f859fbb847f1827
Yaros <Yaroslavhik@yandex.ua>
2021-10-21 11:29:22 +0200

    resolved problems

---
+++ test_blog/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card color="basil">
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="font-weight-bold text-h2 basil--text">
+        BLOG
+      </h1>
+    </v-card-title>
+
+  </v-card>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+/* Helper classes */
+.basil {
+  background-color: #FFFBE6 !important;
+}
+.basil--text {
+  color: #1565C0 !important;
+}
+</style>
