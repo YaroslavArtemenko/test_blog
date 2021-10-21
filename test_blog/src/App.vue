6d3d03789ff46604123a658ed9982c8a4202820e
Yaros <Yaroslavhik@yandex.ua>
2021-10-21 10:50:26 +0200

    added category and changed test data

--- test_blog/src/App.vue
+++ test_blog/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <Header/>
       <Search></Search>
 
     </v-main>
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Header from './components/Header';
 import Search from "./components/Search";
 
 
@@ -19,7 +19,7 @@ export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Header,
     Search
   },
 
--- test_blog/src/components/Article.vue
+++ test_blog/src/components/Article.vue
@@ -4,14 +4,14 @@
       max-width="344"
   >
     <v-card-text>
-      <div>Word of the Day</div>
+      <div>{{ item.category }}</div>
       <p class="text-h4 text--primary">
         {{ item.name }}
       </p>
-      <p>adjective</p>
+      <p>Preview</p>
       <div class="text--primary">
-        relating to or dependent on charity; charitable.<br>
-        "an eleemosynary educational institution."
+        {{ item.previewText }}
+
       </div>
     </v-card-text>
     <v-card-actions>
@@ -20,7 +20,7 @@
           color="teal accent-4"
           @click="reveal = true"
       >
-        Learn More
+        More
       </v-btn>
     </v-card-actions>
 
@@ -34,7 +34,7 @@
           <p class="text-h4 text--primary">
             Origin
           </p>
-          <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+          <p>{{ item.mainText }}</p>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn
